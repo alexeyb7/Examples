7300f1253b7167e37cac0c9877fdba250bf87da0
alexeyb7 <alexeyb7@gmail.com>
2023-04-02 23:56:58 +0300

    Add DZ2

---
+++ dz2_13/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. */
+
+Console.WriteLine("Введите число: ");
+
+string text = Console.ReadLine();
+int i;
+int length = text.Length;
+if ((length > 2) && int.TryParse(text, out i))
+{
+    Console.WriteLine(text[2]);
+}
+else if ((length <= 2) && int.TryParse(text, out i))
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Неправильный ввод");
+}
---
+++ dz2_15/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.*/
+int GetInt()
+{
+    Console.WriteLine("Введите цифру дня недели: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int day = GetInt();
+
+switch (day)
+{
+    case (6):
+        Console.WriteLine("Суббота. Выходной день");
+        break;
+    case (7):
+        Console.WriteLine("Воскресенье. Выходной день");
+        break;
+    case (1):
+        Console.WriteLine("Понедельник. Рабочий день");
+        break;
+    case (2):
+        Console.WriteLine("Вторник. Рабочий день");
+        break;
+    case (3):
+        Console.WriteLine("Среда. Рабочий день");
+        break;
+    case (4):
+        Console.WriteLine("Четверг. Рабочий день");
+        break;
+    case (5):
+        Console.WriteLine("Пятника. Рабочий день");
+        break;
+    default:
+        Console.WriteLine("Неправильный ввод");
+        break;
+}
